5036daa61c6a457c8d70fb36deaec243f29066fb
xX_DarkLorD_Xx <yuzekesu@gmail.com>
2026-02-20 19:54:47 +0100

    Previous was wrong. 00 and 0 is not the same in huffman tree. But they are the same in numerical types, thus the length is needed.

--- demo.cpp
+++ demo.cpp
@@ -4,10 +4,10 @@
 
 int main() {
 	std::vector<char> symbols = { 'A', 'B', 'C', 'D', 'E' };
-	std::vector<int> lengths = { 2,   2,   3,   3,   4 }; // 0000, 0010, 0001, 0101, 0011
+	std::vector<int> lengths = { 2,   2,   3,   3,   4 }; // huffman: 00, 01, 100, 101, 1100
 	HuffmanTree<unsigned, char, int> tree(symbols, lengths);
 	if (tree.Decode(99, 0) == std::nullopt) {
-		std::cout << *tree.Decode(0, 2) << *tree.Decode(2, 2) << *tree.Decode(1, 3) << *tree.Decode(5, 3);
+		std::cout << *tree.Decode(0, 2) << *tree.Decode(1, 2) << *tree.Decode(4, 3) << *tree.Decode(5, 3) << *tree.Decode(12, 4);
 	}
 	return 0;
 }
